5e254253aa5aaf015dbe8e71992fb9019a747f74
Leo Kim <imleokkkk@naver.com>
2025-05-15 17:14:47 +0900

    fix : main question creation bug fix

--- wingterview/src/main/java/com/easyterview/wingterview/interview/repository/InterviewParticipantRepository.java
+++ wingterview/src/main/java/com/easyterview/wingterview/interview/repository/InterviewParticipantRepository.java
@@ -1,12 +1,16 @@
 package com.easyterview.wingterview.interview.repository;
 
+import com.easyterview.wingterview.interview.entity.InterviewEntity;
 import com.easyterview.wingterview.interview.entity.InterviewParticipantEntity;
 import com.easyterview.wingterview.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface InterviewParticipantRepository extends JpaRepository<InterviewParticipantEntity, UUID> {
     Optional<InterviewParticipantEntity> findByUser(UserEntity user);
+
+    List<InterviewParticipantEntity> findByInterview(InterviewEntity interview);
 }
