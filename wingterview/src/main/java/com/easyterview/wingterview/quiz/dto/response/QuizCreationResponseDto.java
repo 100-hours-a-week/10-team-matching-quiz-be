ec5e1a5206b0f156d952e0ec17fdbd8ecc058662
Leo Kim <imleokkkk@naver.com>
2025-06-13 16:16:27 +0900

    fix : parsing problem

--- wingterview/src/main/java/com/easyterview/wingterview/quiz/dto/response/QuizCreationResponseDto.java
+++ wingterview/src/main/java/com/easyterview/wingterview/quiz/dto/response/QuizCreationResponseDto.java
@@ -1,15 +1,25 @@
 package com.easyterview.wingterview.quiz.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.util.List;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
 @ToString
 public class QuizCreationResponseDto {
     private String interviewId;
     private List<QuizItem> questions;
+
+
+    @JsonCreator
+    public QuizCreationResponseDto(
+            @JsonProperty("interviewId") String interviewId,
+            @JsonProperty("questions") List<QuizItem> questions) {
+        this.interviewId = interviewId;
+        this.questions = questions;
+    }
 }
--- wingterview/src/main/java/com/easyterview/wingterview/quiz/dto/response/QuizItem.java
+++ wingterview/src/main/java/com/easyterview/wingterview/quiz/dto/response/QuizItem.java
@@ -1,5 +1,7 @@
 package com.easyterview.wingterview.quiz.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,14 +9,29 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Data // ← @Getter, @Setter, @ToString, @EqualsAndHashCode, @RequiredArgsConstructor 포함
+@Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class QuizItem {
+
     private String difficulty;
     private String question;
     private List<String> options;
     private Integer answerIndex;
     private String explanation;
+
+    @JsonCreator
+    public QuizItem(
+            @JsonProperty("difficulty") String difficulty,
+            @JsonProperty("question") String question,
+            @JsonProperty("options") List<String> options,
+            @JsonProperty("answerIndex") Integer answerIndex,
+            @JsonProperty("explanation") String explanation
+    ) {
+        this.difficulty = difficulty;
+        this.question = question;
+        this.options = options;
+        this.answerIndex = answerIndex;
+        this.explanation = explanation;
+    }
 }
